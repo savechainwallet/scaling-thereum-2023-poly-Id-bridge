6a6a01847329a01726dc31e91faf98097b8a627d
torvald2 <valis.source@gmail.com>
2024-05-21 12:00:43 +0300

    fix

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -14,7 +14,7 @@ export default {
   },
   data(){
     return {
-      qrData:"",
+      qrData:"{\"id\":\"c56a01d6-9614-4d00-878e-b730ef250dcf\",\"typ\":\"application/iden3comm-plain-json\",\"type\":\"https://iden3-communication.io/authorization/1.0/request\",\"thid\":\"c18645e2-aba3-4363-8396-7b83a2401b31\",\"from\":\"did:polyid:polygon:amoy:A74917835AF5A2E353fA40D8F73154ee3b4A6c02\",\"body\":{\"callbackUrl\":\"https://7xa5w6-ip-188-163-94-41.tunnelmole.net/callback?session_id=69e15ca5-dbbf-460c-ae7a-3c95430477ae\",\"reason\":\"KYC Verification\",\"message\":\"Verify Your KYC\",\"scope\":[{\"id\":1,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"date_of_birth\":null}}},{\"id\":2,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"address_country\":null}}},{\"id\":3,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"address_state\":null}}},{\"id\":4,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"address_unit\":null}}},{\"id\":5,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"address_street\":null}}},{\"id\":6,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"address_city\":null}}},{\"id\":7,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"address_zip\":null}}},{\"id\":7,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"email\":null}}},{\"id\":8,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"first_name\":null}}},{\"id\":9,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"last_name\":null}}},{\"id\":10,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"last_name\":null}}},{\"id\":11,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"middle_name\":null}}},{\"id\":12,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"nationality\":null}}},{\"id\":13,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"phone\":null}}},{\"id\":14,\"circuitId\":\"credentialAtomicQuerySigV2\",\"query\":{\"allowedIssuers\":[\"*\"],\"context\":\"https://raw.githubusercontent.com/savechainwallet/scaling-thereum-2023-poly-Id-bridge/main/0idCredentials.jsonld\",\"type\":\"kyc\",\"credentialSubject\":{\"residence\":null}}}]}}",
       sessionId:"",
       timerId:"",
     }
@@ -43,21 +43,12 @@ export default {
 
   },
   async created(){
-   const queryString = window.location.search
-   const urlParams = new URLSearchParams(queryString)
-   const clientId = urlParams.get('client_id')
-   const data = await GetQR(clientId)
+   
    
 
-   this.qrData = data.req
-   this.sessionId = data.session_id
-
-   let timerId = setInterval(this.serveRedirect,1000)
-   this.timerId = timerId
 
   },
   beforeUnmount(){
-    clearInterval(this.timerId)
 
   }
 }
